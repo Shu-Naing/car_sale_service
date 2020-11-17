8fcc8d71b3d89f4176b049786e92d8801c21f6d2
Shu-Naing <shunainghtet.dev@hotmail.com>
2020-11-17 14:33:43 +0630

    connect with templates

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template("about.html")
+
+@app.route('/faq', methods=['GET'])
+def faq():
+    return render_template("faq.html")
+
+@app.route('/contact', methods=['GET'])
+def contact():
+    return render_template("contact.html")
+
+@app.route('/cars', methods=['GET', 'POST'])
+def cars():
+    return render_template("cars.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
