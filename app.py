f3c0fe967bc70347411f454a040875fe66b4fc2a
Shu Naing Htet <shunainghtet@Shus-MacBook-Pro.local>
2020-12-10 00:07:37 +0630

    signup

--- app.py
+++ app.py
@@ -1,18 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 import mysql.connector as mysql
 
-db = mysql.connect(
-    host = "localhost",
-    user = "root",
-    passwd = "rootpassword",
-    db = "cardb"
-)
+def connection():
+    global con, cur
+    con = mysql.connect(host = "localhost",
+                        user = "root",
+                        passwd = "rootpassword",
+                        db = "cardb")
+    cur = con.cursor()
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["POST","GET"])
+def login():
+    # if request.method == "GET":
+        return render_template("login.html")
+
+@app.route("/signup", methods=["POST","GET"])
+def signup():
+    if request.method == "GET":
+        return render_template("signup.html")
+    else:
+        print("Hello")
+        role = request.form['role']
+        print("---------->",role)
+        department = request.form['department']
+        username = request.form['username']
+        address = request.form['address']
+        phone = request.form['phone']
+        password = request.form['password']
+        connection()
+        cur.execute("INSERT INTO employee (role,department,username,address,phone,password) VALUES (%s,%s,%s,%s,%s,%s)", (role,department,username,address,phone,password))
+        con.commit()
+        row = cur.fetchone()
+        return render_template("index", row = row,)
 
 @app.route('/about', methods=['GET'])
 def about():
