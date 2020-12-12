0204107ed14c0b3eec4d2991593ead50a7dde371
Shu Naing Htet <shunainghtet@Shus-MacBook-Pro.local>
2020-12-12 11:31:07 +0630

    admin pandel backend create

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session
 import mysql.connector as mysql
+import os
+import random
 
 def connection():
     global con, cur
@@ -10,34 +12,91 @@ def connection():
     cur = con.cursor()
 
 app = Flask(__name__)
+app.secret_key = os.urandom(16)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template("index.html")
 
+@app.route('/admin_insert', methods=['GET', 'POST'])
+def admin_insert():
+    if request.method == "GET":
+        return render_template("admin_insert.html")
+    else:
+        print("Hello")
+        model = request.form['model']
+        color = request.form['color']
+        price = request.form['price']
+        grade = request.form['grade']
+        car_type = request.form['car_type']
+        engine_power = request.form['engine_power']
+        tax = request.form['tax']
+        img = request.files['img']
+        filename = img.filename
+        fpath = str("static/img/car_photos/"+filename)
+        img.save(fpath)
+        connection()
+        cur.execute("INSERT INTO car (model,color,price,grade,car_type,engine_power,tax_price,photo) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)", (model,color,price,grade,car_type,engine_power,tax,img))
+        con.commit()
+        row = cur.fetchone()
+        return render_template("faq.html", row = row,)
+
+@app.route('/admin_update', methods=['GET', 'POST'])
+def admin_update():
+    # if request.method == "GET":
+        return render_template("admin_update.html")
+    # else:
+
+@app.route('/admin_delete', methods=['GET', 'POST'])
+def admin_delete():
+    # if request.method == "GET":
+        return render_template("admin_delete.html")
+    # else:
+
+
 @app.route("/login", methods=["POST","GET"])
 def login():
-    # if request.method == "GET":
+    if request.method == "GET":
         return render_template("login.html")
+    else:
+        role = request.form['role']
+        session['role'] = role
+        password = request.form['password']
+        connection()
+        cur.execute("SELECT * from employee where emp_role LIKE %s",[role])
+        Password = cur.fetchone()
+        if role == None:
+            return "Username does not exist!!"
+        elif role == "admin" and password == "admin":
+            return render_template("admin_insert.html")
+        else:
+            passwd = Password[3]
+            if password == passwd:
+                return render_template("about.html")
+            else:
+                return "Password is incorrect"
 
 @app.route("/signup", methods=["POST","GET"])
 def signup():
     if request.method == "GET":
         return render_template("signup.html")
     else:
-        print("Hello")
         role = request.form['role']
-        print("---------->",role)
         department = request.form['department']
         username = request.form['username']
         address = request.form['address']
         phone = request.form['phone']
         password = request.form['password']
         connection()
-        cur.execute("INSERT INTO employee (role,department,username,address,phone,password) VALUES (%s,%s,%s,%s,%s,%s)", (role,department,username,address,phone,password))
+        cur.execute("INSERT INTO employee (emp_role,demp,emp_name,emp_address,emp_phone,emp_pass) VALUES (%s,%s,%s,%s,%s,%s)", (role,department,username,address,phone,password))
         con.commit()
         row = cur.fetchone()
-        return render_template("index", row = row,)
+        return render_template("index.html", row = row,)
+
+@app.route("/admin_logout")
+def admin_logout():
+    if request.method== "GET":
+        return redirect('/')
 
 @app.route('/about', methods=['GET'])
 def about():
